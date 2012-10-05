0eafdc43adea15c3e51c390bbc3920c50bbe5b9b
Nathan Trujillo <kelvin.beats@gmail.com>
2012-10-05 12:34:51 -0700

    removed stale files

--- iphoneBup.py
+++ iphoneBup.py
@@ -11,15 +11,15 @@ import shutil
 
 """
 
-This script parses the Manifest.mbdb file which later versions of IOS use to store backup information
 
-It recursively scans for Manifest files,  searches for media and extracts them into folder called media
+This script recursively scans for Manifest files,  searches for media and extracts them into folder of the users choosing.
 
-Run this script in:
-OS X:
-~/Library/Application Support/MobileSync/Backup
-Windows:
-%AppData%\Apple Computer\MobileSync\Backup\
+It then parses the Manifest.mbdb file which later versions of IOS use to store backup information
+
+
+
+Check it out from git, be sure python is in your path, and this script should do the rest, automagically
+
 
 
 This script uses code originally by galloglass on stackoverflow 
@@ -31,10 +31,15 @@ safeMode flag makes no changes to the filesystem, only flip to False if you are 
 
 """
 
+"""
+User defined variables
+"""
 
 outputDir = "Extracted"
-safeMode = False
-IPhoneImageDigestBank = {}mbdx = {}
+safeMode = True
+iPhoneImageDigestBank, mbdx, sizes, extracted = {}, {}, {}, {}
+
+
 
 
 
@@ -44,6 +49,8 @@ IPhoneImageDigestBank = {}mbdx = {}
 	We try to determine home directory -> backup location
 """
 
+
+
 #http://stackoverflow.com/questions/626796/how-do-i-find-the-windows-common-application-data-folder-using-python
 try:
     from win32com.shell import shellcon, shell            
@@ -52,8 +59,131 @@ try:
 except ImportError: # quick semi-nasty fallback for non-windows/win32com case
     homedir = os.path.expanduser("~")
 
+
+
 workingDir = {"DDarwin":"/Volumes/KELUSB-2012/iTunes Backups","Darwin":"~/Library/Application Support/MobileSync/Backup", "Windows":homedir + "\\Apple Computer\\MobileSync\\Backup\\" }
 sizes = {}
+
+
+
+"""
+	We spoof media paths
+"""    	
+def buildiPhoneImageDigestBank():
+	# Media/DCIM/100APPLE/IMG_0001.MOV|JPG
+	print "Spoofing .."   
+	path = "Media/DCIM/"
+	camStart = 100;
+	imgInc = 1;
+	types = {"JPG","MOV"}
+	i = 0
+      
+	for camInc in range(camStart, 105):
+		#print camInc
+		i+=1
+		for imgInc in range( ( ( i * 1000 ) - 1000 ), 1000 * i ):   
+			#print str( camInc ) + "->" + str( imgInc )
+			for t in types:
+				fullpath = path + "{0:03d}".format(camInc) + "APPLE" + "/IMG_" + "{0:04d}".format(imgInc) + "." + t
+				
+   				encryptMe = "MediaDomain-" + fullpath
+  	 			#print encryptMe
+  	 			hash = hashlib.sha1( encryptMe )
+				iPhoneImageDigestBank[hash.hexdigest()] = fullpath;
+
+
+
+"""
+	We extract files from iPhone backup database
+	Mediadomain is most interesting
+"""
+
+def backupFromDatabase( file ):
+
+	mbdb = process_mbdb_file( file )
+	
+	for offset, fileinfo in mbdb.items():
+		if offset in mbdx:
+			fileinfo['fileID'] = mbdx[offset]
+			
+
+			if re.match(ur"^[^A][p][p]", fileinfo['domain']) and fileinfo['filelen'] > 0 and ((fileinfo['mode'] & 0xE000) == 0x8000):# and re.match(ur"[^THM]", fileinfo['filename']):				
+				print fileinfo['domain']
+				backupFileCopy( fileinfo['fileID'], fileinfo['filename'], file )				
+				#extracted[fileinfo['domain']] = extracted.get(fileinfo['domain'],0) + fileinfo['filelen']
+										
+		else:
+			fileinfo['fileID'] = "<nofileID>"
+			print >> sys.stderr, "No fileID found for %s" % fileinfo_str(fileinfo)
+		
+		
+		if fileinfo['filelen'] > 0:
+			if (fileinfo['mode'] & 0xE000) == 0x8000:
+				sizes[fileinfo['domain']] = sizes.get(fileinfo['domain'],0) + fileinfo['filelen']
+
+			
+def backupFileCopy(hash, name, file, dumpDir=outputDir):
+
+
+	#print "I have: " + file
+	#ABSOLUTE MODE
+	dest =  os.path.join( os.getcwd(), dumpDir)
+	#RELATIVE
+	#dest = "./" + dumpDir
+	
+	for p in name.split("/"):
+		dest = os.path.join( dest, p  )					
+
+	DIR =  os.path.split( dest )[0]
+
+	#print "DIR = " + DIR
+	#print "dest = " + dest
+	
+	if not safeMode:
+		try:
+			#print DIR + "?????"
+			os.stat( DIR )
+		except:
+			#print "Making recurse: " + DIR
+			os.makedirs( DIR )
+			
+		src =  os.path.join( os.path.split( file )[0], hash ) 
+		#print src + " -> " +  dest
+		
+		
+		#print "Does " + src + " exist? " + str(os.path.exists( src ))
+		
+		if( os.path.exists( src ) ):						
+			if( os.path.exists( dest ) ):
+				if( os.path.getsize( src ) > os.path.getsize( dest ) ):
+					print ">>> src > dest so copying " + dest
+					shutil.copyfile( src,  dest )
+				#else:
+				#	print ">>> same file..."		
+			else:
+				print ">>> dest doesn't exist, so copying " + src + "->" + dest 	
+				shutil.copyfile( src,  dest )
+		#else:
+		#	print ">>> src doesn't exist :("
+
+
+
+"""
+	We scan through hashes and copy to file system if necessary
+"""
+def extractMediaFromSpoofedHashes( dir ):
+	print "Extracting via hash names from " + dir
+	for hash, path in iPhoneImageDigestBank.items():
+		if( os.path.exists( dir + "//" + hash ) ):
+			backupFileCopy(hash, path, dir + "//pizza", outputDir + "-viahashes" )
+		if( os.path.exists( dir + "//" + hash + ".mdbackup" ) ):
+			backupFileCopy(hash + ".mdbackup", path, dir + "//pizza", outputDir + "-viahashes" )		
+
+
+
+
+
+
 
 
 
@@ -123,35 +253,6 @@ def process_mbdb_file(filename):
 
     return mbdb
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def modestr(val):
     def mode(val):
         if (val & 0x4): r = 'r'
@@ -214,134 +315,7 @@ def fileinfo_str(f, verbose=False):
 	
 	
 	
-	
-	
-def backupFromDatabase( file ):
-
-	mbdb = process_mbdb_file( file )
-	
-	for offset, fileinfo in mbdb.items():
-		if offset in mbdx:
-			fileinfo['fileID'] = mbdx[offset]
-			
-
-			
-			if re.match(ur"^[^A][p][p]", fileinfo['domain']) and fileinfo['filelen'] > 0:# and re.match(ur"[^THM]", fileinfo['filename']):				
-				print fileinfo['domain']
-				backupFileCopy( fileinfo['fileID'], fileinfo['filename'], file )				
-				
-						
-		else:
-			fileinfo['fileID'] = "<nofileID>"
-			print >> sys.stderr, "No fileID found for %s" % fileinfo_str(fileinfo)
-		
-		
-		if fileinfo['filelen'] > 0:
-			if (fileinfo['mode'] & 0xE000) == 0x8000:
-				sizes[fileinfo['domain']] = sizes.get(fileinfo['domain'],0) + fileinfo['filelen']
-
-			
-
-
-
-
-
-
-def backupFileCopy(hash, name, file, dumpDir=outputDir):
-
-
-	#print "I have: " + file
-	#ABSOLUTE MODE
-	dest =  os.path.join( os.getcwd(), dumpDir)
-	#RELATIVE
-	#dest = "./" + dumpDir
-	
-	for p in name.split("/"):
-		dest = os.path.join( dest, p  )					
-
-	DIR =  os.path.split( dest )[0]
-
-	#print "DIR = " + DIR
-	#print "dest = " + dest
-	
-	if not safeMode:
-		try:
-			#print DIR + "?????"
-			os.stat( DIR )
-		except:
-			#print "Making recurse: " + DIR
-			os.makedirs( DIR )
-			
-		src =  os.path.join( os.path.split( file )[0], hash ) 
-		#print src + " -> " +  dest
-		
-		
-		#print "Does " + src + " exist? " + str(os.path.exists( src ))
-		
-		if( os.path.exists( src ) ):						
-			if( os.path.exists( dest ) ):
-				if( os.path.getsize( src ) > os.path.getsize( dest ) ):
-					print ">>> src > dest so copying " + dest
-					shutil.copyfile( src,  dest )
-				#else:
-				#	print ">>> same file..."		
-			else:
-				print ">>> dest doesn't exist, so copying " + src + "->" + dest 	
-				shutil.copyfile( src,  dest )
-		#else:
-		#	print ">>> src doesn't exist :("
-
-
-
-
-
-
-
-
-
-"""
-	We spoof media paths
-"""    	
-def buildIPhoneImageDigestBank():
-	# Media/DCIM/100APPLE/IMG_0001.MOV|JPG
-	print "Spoofing .."   
-	path = "Media/DCIM/"
-	camStart = 100;
-	imgInc = 1;
-	types = {"JPG","MOV"}
-	i = 0
-   
-   
-	for camInc in range(camStart, 105):
-		#print camInc
-		i+=1
-		for imgInc in range( ( ( i * 1000 ) - 1000 ), 1000 * i ):   
-			#print str( camInc ) + "->" + str( imgInc )
-			for t in types:
-				fullpath = path + "{0:03d}".format(camInc) + "APPLE" + "/IMG_" + "{0:04d}".format(imgInc) + "." + t
-				
-   				encryptMe = "MediaDomain-" + fullpath
-  	 			#print encryptMe
-  	 			hash = hashlib.sha1( encryptMe )
-				IPhoneImageDigestBank[hash.hexdigest()] = fullpath;
-
-"""
-	We scan through hashes and copy to file system if necessary
-"""
-def extractMediaFromSpoofedHashes( dir ):
-	print "Extracting via hash names from " + dir
-	for hash, path in IPhoneImageDigestBank.items():
-		if( os.path.exists( dir + "//" + hash ) ):
-			backupFileCopy(hash, path, dir + "//pizza", "Extract-test" )
-		if( os.path.exists( dir + "//" + hash + ".mdbackup" ) ):
-			backupFileCopy(hash + ".mdbackup", path, dir + "//pizza", "Extract-test" )		
-		
-	
-	
-	    	
-	    	
-	    	
-	    	
+ 	
 	    	
 	    	
 	    	
@@ -361,8 +335,7 @@ if __name__ == '__main__':
 	# change to iTunes backup directory, wherever that may be
 	os.chdir(os.path.expanduser(workingDir[ platform.system() ]))
 
-	buildIPhoneImageDigestBank()
-	#exit();   
+	buildiPhoneImageDigestBank()   
 		
 	if not safeMode:
 		try:
@@ -376,7 +349,7 @@ if __name__ == '__main__':
 		for ff in files:
 			if re.match(ur"((?!shot).)*$", root):  #ignore snapshot directories
 				if re.match(ur"^Manifest\.mbdb$", ff):
-					print "^-^" + root + "\\" + ff
+					print " ^-^ " + root + "\\" + ff
 					backupFromDatabase(os.path.join(root, ff))
 		if re.match(ur"((?!Extract).)*$", root ) and root != ".":# we don't check our extracted folder, or current folder
 			print root
@@ -386,6 +359,24 @@ if __name__ == '__main__':
 					
 	for domain in sorted(sizes, key=sizes.get):
 		if( sizes[domain] > 1024 * 1024 ):
-			print "%-60s (%dMB)" % (domain, int(sizes[domain]/1024/1024))
-					
+			print "%-60s Extracted: (%dMB)" % (domain, int(sizes[domain]/1024/1024))
+
+
+
+
+
+
+"""
+File notes
+SMS / Text messages	1-6	sms.db	3d0d7e5fb2ce288813306e4d4636395e047a3d28	SQLite 3
+Contacts / address book	2-6	AddressBook.sqlitedb	31bb7ba8914766d4ba40d6dfb6113c8b614be442	SQLite 3
+Calendar	2-6	Calendar.sqlitedb	2041457d5fe04d39d0ab481178355df6781e6858	SQLite 3
+Notes	4-6	notes.sqlite	ca3bc056d4da0bbf88b5fb3be254f3b7147e639c	SQLite 3
+Call history	4-6	call_history.db	2b2b0084a1bc3a5ac8c27afdf14afb42c61a19ca	SQLite 3
+Locations	4 - 6	consolidated.db	4096c9ec676f2847dc283405900e284a7c815836	SQLite 3
+dsfdasf 12b144c0bd44f2b3dffd9186d3f9c05b917cee25  lkjsadflkjasdf iPhoto
+adsfasdf b03b6432c8e753323429e15bc9ec0a8040763424 lkjsdf iPhoto backup
+
+photos, SMS, call log and contacts
+"""					
 					
